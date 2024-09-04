8bb3c0653a6440ac6392b014661dc98795791d15
danielore98 <71668153@continental.edu.pe>
2024-09-04 11:46:28 -0500

    Se agrega el archivo Ejercicio 3_10

---
+++ Ejercicio_3_10.py
@@ -0,0 +1,17 @@
+# Calculo del Saldo
+
+#INICIO
+
+print('Dame saldo actual: ', end="")
+Saldo = float(input())
+
+#Empieza la condicion:
+if(Saldo < 10000.00):
+    Saldo = Saldo*(1 + 0.03)
+else:
+    Saldo = Saldo*(1 + 0.04)
+#Fin del if
+
+print("Saldo final es %5.2f" %Saldo)
+
+#FIN
